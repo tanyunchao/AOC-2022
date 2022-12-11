bd719b22c34457e9a516163026b61b18bfdabbf1
tanyunchao <63942020+tanyunchao@users.noreply.github.com>
2022-12-11 15:27:00 +0800

    adding day9

---
+++ day9/day9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class day9 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/Users/yunchao/Library/CloudStorage/OneDrive-NationalUniversityofSingapore/Documents/AdventOfCode/AOC-2022/day9/input2.txt");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()){
+        
+        }
+        sc.close();
+    }
+}
